a58500a17f10f7797107da29bd0d5106559226f7
Valentine Butler-Aneke <123885094+ButlerVal@users.noreply.github.com>
2025-10-21 11:10:53 +0100

    Change import from tensorflow.keras to keras

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 from PIL import Image
 import tensorflow as tf
-from tensorflow.keras.models import load_model
+from keras.models import load_model
 from tensorflow.keras.applications.efficientnet import preprocess_input as eff_preprocess
 import cv2
 import warnings
